3d00b99ca8f2061b68e13980302797aef8b759ac
Charlot <7wangsong@gmail.com>
2019-12-05 15:26:51 +0800

    add pages

--- operation_resource_tool/urls.py
+++ operation_resource_tool/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 from django.conf.urls import url
 
-urlpatterns=[
- url(r'^converter/$',views.converter, name='converter'),
- url(r'^upload/$', views.converter_upload,name='upload')
+urlpatterns = [
+    url(r'^converter/$', views.converter, name='converter'),
+    url(r'^upload/$', views.converter_upload, name='upload'),
+    url(r'^convert/$', views.convert, name='convert'),
+
 ]
--- operation_resource_tool/views.py
+++ operation_resource_tool/views.py
@@ -1,19 +1,23 @@
-from django.shortcuts import render,redirect
-
+from django.shortcuts import render, redirect
 
 
 def index(request):
-    #return render(request, 'index.html')
+    # return render(request, 'index.html')
     return redirect('converter')
 
+
 def converter(request):
-    context ={'files':{
-        'product_attribute':[{'name':'a.xlsx','value':'data/a-uuid.a.xlsx'}],
-        'item_project':[{'name':'a.xlsx','value':'data/a-uuid.a.xlsx'}]
+    context = {'files': {
+        'product_attribute': [{'name': 'a.xlsx', 'value': 'data/a-uuid.a.xlsx'}],
+        'item_project': [{'name': 'a.xlsx', 'value': 'data/a-uuid.a.xlsx'}]
     }}
-    return render(request, 'converter.html',context=context)
+    return render(request, 'converter.html', context=context)
+
+
+def convert(request):
+    return render(request, 'convert_result.html', context={'message': '处理成功'})
 
 
 def converter_upload(request):
-
+    files = request.FILES
     return redirect('converter')
